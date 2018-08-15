50e6ff103ece0acdb25d7c833f30f22b5c894d88
sletlape <sletlape@42.FR>
2018-08-15 17:56:10 +0200

    more work...

---
+++ src/main/java/controller/CLIController.java
@@ -0,0 +1,9 @@
+package controller;
+
+public class CLIController {
+    public CLIController(){
+        System.out.println("####################################################################");
+        System.out.println("Running the Command Line Interface!");
+        System.out.println("####################################################################");
+    }
+}
