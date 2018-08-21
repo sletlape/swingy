345db594630da965db65c075ce1cba1a891bc382
sletlape <sletlape@42.FR>
2018-08-21 17:12:01 +0200

    shaggy's help

---
+++ src/main/java/controller/Entity/PlayerController.java
@@ -0,0 +1,29 @@
+package controller.Entity;
+
+import model.Artifacts.Artifact;
+import model.LivingElements.Hero;
+import utils.Formulas;
+
+import java.awt.*;
+
+public class PlayerController {
+    Hero hero;
+
+    void levelUp(int points) {
+    }
+
+    void collectAritfact(Artifact artifact) {
+    }
+
+    void registerHero(Hero hero){
+        this.hero = hero;
+        putHeroInTheCenter();
+    }
+
+    private void putHeroInTheCenter() {
+        Point heroPoint = hero.getPoint();
+        int mapSize = Formulas.getMapSize(hero.getLevel());
+        heroPoint.x = mapSize /2;
+        heroPoint.y = mapSize /2;
+    }
+}
